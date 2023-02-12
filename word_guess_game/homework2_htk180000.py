02fbc162b1d01ff677f6aeab59cb975b322b550b
Henry Kim <6henrykim@gmail.com>
2023-02-11 19:52:33 -0600

    Update homework2_htk180000.py

--- word_guess_game/homework2_htk180000.py
+++ word_guess_game/homework2_htk180000.py
@@ -15,14 +15,16 @@ from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
 from random import seed
 from random import randint
+from time import time
 
 
 def validate_guess(guess):
     """
-    Checks if the guess is a single letter or ! and recursivelhy prompts the user again if it is not
+    Checks if the guess is a single letter or ! and recursively prompts the user again if it is not
     :param guess: input from the user
-    :return: a single char
+    :return: a single char that is a letter or !
     """
+    guess = guess.lower()
     if (len(guess) == 1 and guess.isalpha()) or guess == '!':
         return guess
     else:
@@ -31,41 +33,96 @@ def validate_guess(guess):
         return validate_guess(new_guess)
 
 
+def choose_word(word_list):
+    """
+    Randomly choose a word from the given list
+    :param word_list: a list of word strings to choose from
+    :return: a random word string in the list
+    """
+    seed(time())
+    return word_list[randint(0, len(word_list) - 1)]
+
+
+def display_word(word, guesses):
+    """
+    Prints letters of a word that have been guessed and underscores for the letters that haven't been guessed yet.
+    :param word: the word the user is trying to guess
+    :param guesses: a list of characters the user has guessed
+    """
+    progress = ""
+    for letter in word:
+        if letter in guesses:
+            progress += letter + " "
+        else:
+            progress += "_ "
+    print(progress)
+
 
 def guessing_game(word_list):
     """
     Prompts the user to guess the letters in a random word from the word_list. The user starts with 5 points, gains 1
     point for each correctly guessed letter, and loses 1 point for each incorrectly guess. The game ends when the user
     has a negative score or when they enter an !
-
-    :return: final score
     """
-    score = 5
 
-    # Randomly choose a word
-    # TODO: Use time as the random seed
-    seed(1)
-    target_word = word_list[randint(0, len(word_list))]
-    print(target_word)
+    print("Let's play a word guessing game!")
+
+    # Set up game loop
+    guessed_this_round = []
+    score = 5
+    target_word = choose_word(word_list)
 
     # Game loop
-    keep_playing = True
-    guesses_this_round = {}
+    while score >= 0:
+        # Display user progress on guessing the word
+        display_word(target_word, guessed_this_round)
 
-    while keep_playing:
-        # TODO: print the underscores
+        # Get the user input
+        guess_input = validate_guess(input("Guess a letter: "))
 
-        letter_guess = input("Guess a letter: ").lower()
-        letter_guess = validate_guess(letter_guess)
-        if letter_guess == '!':
-            keep_playing = False
+        # End game loop if user entered !
+        if guess_input == '!':
+            break
+
+        # Make sure user has not tried this guess already for this word
+        if guess_input in guessed_this_round:
+            # Tell user they already tried this letter and skip this iteration
+            print("You already guessed", guess_input, "this round. Try a different letter")
+            continue
         else:
-            # Check answer, adjust score
-            # Get the next word
+            guessed_this_round.append(guess_input)
 
+        # Process whether this new guess is correct or not
+        if guess_input in target_word:
+            score += 1
+            print("Right! Score is", score)
+        else:
+            score -= 1
+            print("Sorry, incorrect. Score is", score)
 
-    # Return the score
-    return score
+        # Check if all letters in the word have been guessed
+        complete = True
+        for letter in target_word:
+            if letter not in guessed_this_round:
+                # Incomplete if any letter in the target word hasn't been guessed yet
+                complete = False
+
+        # Prepare for next round once all letters have been guessed
+        if complete:
+            print("You solved it!")
+            display_word(target_word, guessed_this_round)
+            print()
+            print("current score:", score)
+            print()
+            print("Guess another word")
+            # Reset the target word and list of guesses
+            target_word = choose_word(word_list)
+            guessed_this_round = []
+
+    # Display the score
+    print()
+    print("Game over")
+    print("Final score:", score)
 
 
 # Main execution
@@ -79,8 +136,4 @@ if __name__ == '__main__':
     # TODO: Process the file
 
     # Play the guessing game
-    print("Let's play a word guessing game!")
-    final_score = guessing_game(['cat', 'dog'])
-    print()
-    print("Final score:", final_score)
-
+    guessing_game(['coot', 'dog'])
